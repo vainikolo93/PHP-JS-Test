0201f2c5eec60ffbf3d526378ea5d300e866756c
Nancy Vainikolo <nancyvainikolo@gmail.com>
2016-09-15 22:36:06 +1200

    php_update

--- default.php
+++ default.php
@@ -7,7 +7,13 @@
         <link rel="stylesheet" type="text/css" href="./css/style.css">
     </head>
     <body>
+        <SCRIPT TYPE="text/javascript" src="./TestLogin/fb.js"> 
+        <div id="content" style="width: 70%; float: left;">
         <h1>Hello World</h1>
+        </div>
+        <div id="content" style="width: 20%; float: right;">
+            <<div class="fb-login-button"></div>
+        </div>
     </body>
         <!--jQuery Script-->
         <script src="./node_modules/jquery/dist/jquery.min.js"></script>
